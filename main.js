439a8fcb5b0f50dab08da688a18edfa0f837b086
VARAM3NDUM <99696666+VARAM3NDUM@users.noreply.github.com>
2022-06-15 10:06:57 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("My First Console Project");
+
+name1 = "Ranbir";
+name2 = "Kapoor";
+var fullname = name1.concat(name2);
+console.log(fullname)
+
+var a = 5;
+var b = 10;
+
+var sum = a+b;
+console.log(sum);
+
+var dif = b-a;
+console.log(dif);
+
+var times = a*b;
+console.log(times);
+
+var divide = b/a;
+console.log(divide);
